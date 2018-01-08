3192d39a713174e96fcb45e2d3564935890cfd49
constancecchen <constance.chen.3@gmail.com>
2018-01-07 23:37:47 -0800

    Fix responsive issues - Homepage - h1 font size - Footer - column collapsing - Nav - add mobile nav menu, animation courtesy of https://github.com/jonsuh/hamburgers - Grid - switch from px to em/rem - Small HTML fixes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  document.getElementById('js-mobile-nav-toggle').addEventListener('click', function() {
+    this.classList.toggle('is-active');
+    this.nextSibling.classList.toggle('is-active');
+  });
+
 });
