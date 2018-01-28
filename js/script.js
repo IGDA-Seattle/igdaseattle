866cfe667a560f1dd9d06d786cbce80bfa7eacf4
constancecchen <constance.chen.3@gmail.com>
2018-01-27 21:58:33 -0800

    Attempt to add smoother loading experience for embeds/iframes

--- js/script.js
+++ js/script.js
@@ -36,3 +36,35 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 });
+
+/**
+ * Smoother load-in transitions for iframes
+ */
+document.onreadystatechange = function () {
+  // Normal iframes
+  if (document.readyState === 'interactive') {
+    var iframes = document.querySelectorAll('iframe');
+
+    for (var i = 0; i < iframes.length; i++) {
+      iframes[i].style.opacity = 0;
+
+      iframes[i].addEventListener('load', function (e) {
+        this.classList.add('is-fading-in');
+        this.style.animationDuration = '.6s';
+      });
+    }
+  }
+  // Facebook embeds
+  if (document.readyState === 'complete') {
+    if (typeof FB !== 'undefined') {
+      FB.Event.subscribe('xfbml.render', function(response) {
+        var iframes = document.querySelectorAll('.fb_iframe_widget iframe');
+
+        for (var i = 0; i < iframes.length; i++) {
+          iframes[i].classList.add('is-fading-in');
+          iframes[i].style.animationDuration = '1s';
+        }
+      });
+    }
+  }
+}
