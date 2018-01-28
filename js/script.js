86b0450315ebcaf4dbad13dcbe57cc96749b60b5
constancecchen <constance.chen.3@gmail.com>
2018-01-27 21:56:21 -0800

    Add page fade in/out transitions for a smoother UX
    - Credit to the Urban Influence folks for getting me addicted to nice things
    - Also removed previous bodyClasses override, as it wasn't being used

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  /**
+   * Page transitions
+   */
+
+  // Fade in
+  setTimeout(function() {
+    document.body.classList.remove('is-fading-in');
+  }, 500)
+
+  // Fade out
+  var relativeLinks = document.querySelectorAll('a[href^="/"]');
+
+  for (var i = 0; i < relativeLinks.length; i++) {
+    relativeLinks[i].addEventListener('click', function (e) {
+      if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey) { return; }
+
+      e.preventDefault();
+      document.body.classList.add('is-fading-out');
+      var link = this.href;
+
+      setTimeout(function () {
+        window.location = link;
+      }, 500);
+    });
+  }
+
+  /**
+   * Mobile nav
+   */
+
   document.getElementById('js-mobile-nav-toggle').addEventListener('click', function() {
     this.classList.toggle('is-active');
     this.nextSibling.classList.toggle('is-active');
